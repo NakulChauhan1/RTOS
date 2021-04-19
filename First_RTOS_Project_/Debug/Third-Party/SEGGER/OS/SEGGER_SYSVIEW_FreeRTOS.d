c5566e2bb74ea0e3ebc3173a8d9225bc8f39d6c4
Nakul Chauhan <nakulchauhan111@gmail.com>
2021-04-19 19:08:20 +0530

    removed unused file and added new project

---
+++ First_RTOS_Project_/Debug/Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.d
@@ -0,0 +1,50 @@
+Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.o: \
+ ../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.c \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/FreeRTOSConfig.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/list.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/Global.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h \
+ ../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/FreeRTOSConfig.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/Global.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
+
+../Third-Party/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
