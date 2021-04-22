76b3680b63e390b78768170c66708f5546008e0d
Nakul Chauhan <nakulchauhan111@gmail.com>
2021-04-22 12:37:37 +0530

    garbbage commit

--- First_RTOS_Project_/Debug/Third-Party/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,8 +0,0 @@
-Third-Party/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- ../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
-
-/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
--- First_RTOS_Project_/Debug/Third-Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,8 +0,0 @@
-Third-Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../Third-Party/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h
-
-/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/SEGGER/SEGGER_RTT.h:
-
-/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/First_RTOS_Project_/Third-Party/SEGGER/Config/SEGGER_RTT_Conf.h:
