f845b4ff685288785800777b1675093cd7db5009
Nakul Chauhan <nakulchauhan111@gmail.com>
2021-04-22 12:35:49 +0530

    added new project

---
+++ Button_and_Led_Notify/Debug/Third-Party/FreeRTOS/org/Source/timers.d
@@ -0,0 +1,38 @@
+Third-Party/FreeRTOS/org/Source/timers.o: \
+ ../Third-Party/FreeRTOS/org/Source/timers.c \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/FreeRTOSConfig.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/projdefs.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/portable.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/list.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/queue.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/task.h \
+ /home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/timers.h
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/FreeRTOSConfig.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/projdefs.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/portable.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/list.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/queue.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/task.h:
+
+/home/nakul/Desktop/Workspace/RTOS_WORKSPACE/Button_and_Led_Notify/Third-Party/FreeRTOS/org/Source/include/timers.h:
